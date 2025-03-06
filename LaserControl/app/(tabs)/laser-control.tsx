c155bde90d2d332dad85df9742d51a437f974092
doctored11 <edit.shultz@yandex.ru>
2025-03-06 18:35:15 +0300

    turn on/off toggle

--- LaserControl/app/(tabs)/laser-control.tsx
+++ LaserControl/app/(tabs)/laser-control.tsx
@@ -1,9 +1,33 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, Switch, StyleSheet, Alert } from 'react-native';
+
+const ESP32_IP = "http://192.168.31.110";  
 
 export default function LaserControlScreen() {
+  const [isLaserOn, setIsLaserOn] = useState(false);
+
+  const toggleLaser = async () => {
+    const newState = !isLaserOn;
+    setIsLaserOn(newState);
+
+    try {
+      const response = await fetch(`${ESP32_IP}/toggle?state=${newState ? 1 : 0}`);
+      if (!response.ok) throw new Error("Ошибка запроса");
+    } catch (error) {
+      Alert.alert("Ошибка", "Не удалось связаться с ESP32");
+      setIsLaserOn(!newState); 
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Экран управления лазером</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Лазер {isLaserOn ? 'Включен' : 'Выключен'}</Text>
+      <Switch value={isLaserOn} onValueChange={toggleLaser} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
+  text: { fontSize: 20, marginBottom: 10 },
+});
